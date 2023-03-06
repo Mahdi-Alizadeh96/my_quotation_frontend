ecccb3635355cc6835e45105f4705bf4795608b8
Mahdi Alizadeh <mahdi.alizadeh.sp@gmail.com>
2023-03-07 01:52:55 +0330

    test sass configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = (phase, {defaultConfig}) => {
+  if ('sassOptions' in defaultConfig) {
+      defaultConfig['sassOptions'] = {
+        prependData: `
+                      @import "./src/styles/variablesStyles.module.scss";
+                      @import "./src/styles/mixins.module.scss";
+                      `,
+      }
+  }
+  return {...nextConfig};
+}
